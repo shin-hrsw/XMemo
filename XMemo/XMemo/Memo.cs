88c70a2fbcadf8598d754b84ca01123e89f926d2
Shintaro Hirasawa <nobizo@gmail.com>
2017-10-20 08:55:51 +0900

    Memoクラスにプロパティを追加

--- XMemo/XMemo/Memo.cs
+++ XMemo/XMemo/Memo.cs
@@ -8,6 +8,20 @@ namespace XMemo
     public class Memo
     {
         #region プロパティ
+        /// <summary>
+        /// 作成日
+        /// </summary>
+        public DateTime Date { get; private set; }
+
+        /// <summary>
+        /// 件名
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// 本文
+        /// </summary>
+        public string Text { get; set; }
         #endregion
     }
 }
