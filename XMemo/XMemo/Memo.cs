7f109bc3d1703a093c67936bed79edbd0d1367a2
Shintaro Hirasawa <nobizo@gmail.com>
2017-10-20 08:54:04 +0900

    プロジェクト ファイルを追加します。

---
+++ XMemo/XMemo/Memo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMemo
+{
+    public class Memo
+    {
+        #region プロパティ
+        #endregion
+    }
+}
