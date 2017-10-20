efac99b8da49e2c3ae4aa875d12fb3df8f60f343
Shintaro Hirasawa <nobizo@gmail.com>
2017-10-20 09:51:43 +0900

    MemoHolderクラスを追加

---
+++ XMemo/XMemo/MemoHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMemo
+{
+    public class MemoHolder
+    {
+        #region プロパティ
+        public static MemoHolder Current => new MemoHolder();
+
+        public Memo Memo { get; set; }
+        #endregion
+    }
+}
