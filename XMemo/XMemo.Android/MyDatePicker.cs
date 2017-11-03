d943379342703dab75ba79f0f68d72d98ddc1f46
Shintaro Hirasawa <nobizo@gmail.com>
2017-11-03 11:57:01 +0900

    日付選択ダイアログの実装

---
+++ XMemo/XMemo.Android/MyDatePicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XMemo.Droid
+{
+    class MyDatePicker : DialogFragment, DatePickerDialog.IOnDateSetListener
+    {
+        public DateTime InitialDate { get; set; }
+        public event EventHandler<MyDatePickerEventArgs> Selected;
+
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            return new DatePickerDialog(Activity, this,
+                this.InitialDate.Year, this.InitialDate.Month - 1, this.InitialDate.Day);
+        }
+
+        #region IOnDateSetListenerの実装
+        public void OnDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+        {
+            var selectedDate = new DateTime(year, monthOfYear + 1, dayOfMonth);
+            Selected?.Invoke(this, new MyDatePickerEventArgs(selectedDate));
+        }
+        #endregion
+    }
+}
---
+++ XMemo/XMemo.Android/MyDatePickerEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace XMemo.Droid
+{
+    class MyDatePickerEventArgs : EventArgs
+    {
+        public DateTime SelectedDate { get; private set; }
+
+        #region コンストラクタ
+        public MyDatePickerEventArgs(DateTime selected)
+        {
+            SelectedDate = selected;
+        }
+        #endregion
+    }
+}
